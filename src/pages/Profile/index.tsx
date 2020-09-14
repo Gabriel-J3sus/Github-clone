31eaaf9d6665feebdc775c5108ce9aaa956b7ed0
Gabriel-J3sus <gabr.jesus001@gmail.com>
2020-09-14 18:00:03 -0300

    Profile page finished

--- src/pages/Profile/index.tsx
+++ src/pages/Profile/index.tsx
@@ -5,13 +5,37 @@ import {
   Main,
   LeftSide,
   RightSide,
+  Repos,
+  CalendarHeading,
+  RepoIcon,
+  Tab,
 } from './styles';
 
 import ProfileData from '../../components/ProfileData';
+import RepoCard from '../../components/RepoCard';
+import RandomCalendar from '../../components/RandomCalendar';
 
 const Profile: React.FC = () => {
+  const TabContent = () =>  (
+    <div className="content">
+      <RepoIcon />
+      <span className="label"> Repositories </span>
+      <span className="number"> 17 </span>
+    </div>
+  )
+
   return (
     <Container>
+      <Tab className="desktop">
+        <div className="wrapper">
+          <span className="offset"></span>
+          <TabContent />
+        </div>
+        
+
+        <span className="line" />
+      </Tab>
+
       <Main>
         <LeftSide>
           <ProfileData 
@@ -29,6 +53,34 @@ const Profile: React.FC = () => {
         </LeftSide>
 
         <RightSide>
+          <Tab className="mobile">
+            <TabContent />
+            <span className="line" />
+          </Tab>
+
+          <Repos>
+            <h2>Random Repos</h2>
+
+            <div>
+              {[1, 2, 3, 4, 5, 6].map(n => (
+                <RepoCard 
+                  key={n}
+                  username={"Gabriel-J3sus"}
+                  reponame={"Github-Clone"}
+                  description={"Clone da interface do Github"}
+                  language={n % 3 === 0 ? 'JavaScript' : 'TypeScript'}
+                  stars={2}
+                  forks={5}
+                />
+              ))}
+            </div>
+          </Repos>
+
+          <CalendarHeading>
+            Random calendar (do not represent actual data)
+          </CalendarHeading>
+          
+          <RandomCalendar />
 
         </RightSide>
       </Main>

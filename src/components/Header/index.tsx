4f37882b794e71a1748fd6947f04c28880d7e398
Gabriel-J3sus <gabr.jesus001@gmail.com>
2020-09-16 09:08:36 -0300

    Pesquisa com dados da API

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,4 +1,5 @@
-import React from 'react';
+import React, { useState } from 'react';
+import { useNavigate } from 'react-router-dom';
 
 import { 
   Container,
@@ -6,12 +7,36 @@ import {
   SearchForm,
 } from './styles';
 
-const Header: React.FC = () => {
+import { ThemeName } from '../../styles/Themes';
+
+interface Props {
+  themeName: ThemeName;
+  setThemeName: (newName: ThemeName) => void;
+}
+
+const Header: React.FC<Props> = ({ themeName, setThemeName }) => {
+  const [search, setSearch ] = useState('');
+  const navigate = useNavigate();
+
+  function handleSubmit(event: React.FormEvent) {
+    event.preventDefault();
+
+    navigate('/' + search.toLowerCase().trim());
+  }
+
+  function toggleTheme() {
+    setThemeName(themeName === 'light' ? 'dark' : 'light');
+  }
+
   return (
     <Container>
-      <GithubLogo />
-      <SearchForm>
-        <input placeholder="Enter Username ou Repo..." />
+      <GithubLogo onClick={toggleTheme} />
+      <SearchForm onSubmit={handleSubmit}>
+        <input 
+          placeholder="Enter Username ou Repo..."
+          value={search}
+          onChange={e => setSearch(e.currentTarget.value)}
+        />
       </SearchForm>
     </Container>
   );
